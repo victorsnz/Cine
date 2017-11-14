39a13cecb65735906320543957996d9e6af116d5
Victor Sanchez <victor_sanchez@outlook.com>
2017-11-14 01:56:02 -0300

    Actualizacion de Funcion

--- src/cine/Funcion.java
+++ src/cine/Funcion.java
@@ -5,23 +5,48 @@
  */
 package cine;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Usuario
+ * @author Victor Sanchez
  */
+
 public class Funcion {
     private String diaSemana;
     private int duracion;
     private int horaInicio;
     private int numero;
+     private Sala sala;                   //cambio linea 17 funcion
+    private ArrayList<Entrada> entradas; //cambio linea 18 funcion
 
-    public Funcion(String diaSemana, int duracion, int horaInicio, int numero) {
+    // Constructor
+    public Funcion(String diaSemana, int duracion, int horaInicio, int numero,
+                   Sala sala, ArrayList<Entrada> entradas ) {
         this.diaSemana = diaSemana;
         this.duracion = duracion;
         this.horaInicio = horaInicio;
         this.numero = numero;
+        this.sala = sala;
+        this.entradas = entradas;
+    }
+    
+    // Constructor
+    public Funcion(String diaSemana, int duracion, int horaInicio, int numero,
+                   Sala sala, Entrada entrada ) {
+        this.diaSemana = diaSemana;
+        this.duracion = duracion;
+        this.horaInicio = horaInicio;
+        this.numero = numero;
+        this.sala = sala;
+        this.entradas = new ArrayList<>();
+        this.addEntrada(entrada);
     }
 
+     public void addEntrada(Entrada entrada) {
+        this.entradas.add(entrada);
+    }
+     
     public String getDiaSemana() {
         return this.diaSemana;
     }
@@ -54,9 +79,31 @@ public class Funcion {
         this.numero = numero;
     }
     
+    //cambio linea 58 funcion
+    public Sala getSala() {
+        return this.sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+    //linea 66
+     public ArrayList<Entrada> getEntradas() {
+        return this.entradas;
+    }
+
+    public void setEntradas(ArrayList<Entrada> entradas) {
+        this.entradas = entradas;
+    }
+    
+    @Override
+    public String toString() {
+        return "Datos de la función: " + "Falta de hacer";
+    }
+    
 //    //Calcular disponibilidad
-//    public int calcularDisponibilidad (){    
-//    }
+//    public int calcularDisponibilidad (){ 
+        //return this.capacidadSala()
 //    
 //    //Capacidad Sala
 //    public int capacidadSala(){
@@ -66,5 +113,6 @@ public class Funcion {
 //    public boolean estaEnCurso(){
 //        if(this.getHoraInicio > )
 //    }
-    
+//     public boolean hayLugar() {
+//        return true;   
 }
